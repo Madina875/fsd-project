57c2c8f03de18550f38ff725e80b39cd0355e1fc
madina875 <evaevasmq@gmail.com>
2025-09-23 13:00:31 +0500

    hmw

--- src/pages/about/ui/About.tsx
+++ src/pages/about/ui/About.tsx
@@ -1,10 +1,12 @@
 import { memo } from "react";
 import { AboutHero } from "../../../widgets/about-hero";
+import { AboutInfo } from "../../../widgets/about-info";
 
 export const About = memo(() => {
   return (
     <div>
       <AboutHero />
+      <AboutInfo />
     </div>
   );
 });
---
+++ src/widgets/about-info/ui/AboutInfo.tsx
@@ -0,0 +1,30 @@
+import { memo } from "react";
+import img from "@/shared/assets/Group 3.png";
+
+export const AboutInfo = memo(() => {
+  return (
+    <div className="container h-[480px] pl-2 gap-2 flex mt-[57px]">
+      <div>
+        <h2 className="mb-[31px] text-4xl font-medium w-150">
+          Seedra helps to grow fast and efficiant
+        </h2>
+        <p className="w-120 text-[17px] text-gray-800">
+          SEEDRA Spinach Seeds - contains 600 seeds in 2 Packs and professional
+          instructions created by PhD Helga George <br />
+          <br /> Be sure of our quality - the freshest batches of this season.
+          Non-GMO, Heirloom - our seeds were tested and have the best
+          germination ratings.
+          <br /> Your easy growing experience is our guarantee Spinach commom
+          culinary uses: salads, soups, smoothies, lasagne, pizza, pies,
+          risotto, and more <br />
+          <br /> Proudly sourced in the USA - our garden seeds are grown,
+          harvested, and packaged in the USA. We support local farmers and are
+          happy to produce this American-made product
+        </p>
+      </div>
+      <div>
+        <img src={img} className="object-center object-cover" alt="" />
+      </div>
+    </div>
+  );
+});
